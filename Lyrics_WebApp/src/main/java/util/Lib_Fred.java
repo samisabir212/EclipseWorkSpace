539052002667dd6eaeecaee77beb80726d9d2241
Sami Sabir <samisabir212@gmail.com>
2018-01-04 18:47:47 -0500

    lyrics webapp project testing 'Master' version 1

---
+++ Lyrics_WebApp/src/main/java/util/Lib_Fred.java
@@ -0,0 +1,34 @@
+package util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Lib_Fred{
+	
+	//lib is for reusable methods across page class and test class
+	
+	
+	static WebDriver driver;
+
+	
+	public static WebElement findelement(WebDriver driver, String xpath) {
+
+		WebElement a = driver.findElement(By.xpath(xpath));
+
+		return a;
+	}
+
+	public static void sleep(WebDriver driver, int sec) throws InterruptedException {
+
+		Thread.sleep(sec * 1000);
+	}
+	
+	public static String printTitle(WebDriver driver) {
+		String a=driver.getTitle();
+		
+		return a;
+		
+	}
+
+}
---
+++ Lyrics_WebApp/src/test/java/tests/HomePageTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import generic.Common_API;
+import pages.HomePage;
+
+public class HomePageTest extends Common_API {
+	
+	public HomePageTest() throws IOException{
+		super();
+		
+	}
+	
+	public HomePage hp = new HomePage();
+
+	
+	
+	@Test(enabled = false)
+	public void searchForLyrics(){
+		//enter method from pages class that has a function to send data to lyrics searchfield
+		
+		
+		hp.sendDataToSearchField(driver,"cream");
+		hp.clickSearchButton(driver);
+		
+	}
+	
+	
+	@Test(enabled = true)
+	public void testCommonAPIMethods() {
+		
+		hp.searchByArttist();
+	}
+	
+
+}
