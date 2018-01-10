018d56c84ce0866689384a65118917b689315409
Sami Sabir <samisabir212@gmail.com>
2018-01-09 22:10:23 -0500

    testngCUucmber

---
+++ TestNGCucumber/src/main/java/CucumberTestNG/TestNGCucumber/App.java
@@ -0,0 +1,13 @@
+package CucumberTestNG.TestNGCucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
